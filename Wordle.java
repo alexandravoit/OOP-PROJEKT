807693bbea008f353b7bb6ffeb1ca809a629d770
Alexandra Voit <145194484+alexandravoit@users.noreply.github.com>
2024-03-20 13:35:09 +0200

    Update Wordle.java

--- Wordle.java
+++ Wordle.java
@@ -6,31 +6,37 @@ public class Wordle {
         // ALGVÄÄRTUSTUSED
         int guessCounter = 1;
         boolean äraArvatud = false;
-        sõnaValija sõnaValija = new sõnaValija();
-        String[] sõnapaar = sõnaValija.valiSõna();
 
-        String tähendus = sõnapaar[1];
-        String sõna = sõnapaar[0];
-        //String sõna = "moonisai"; // 8 characteri, jätan alles veel testimiseks
+        sõnaValija sõnaValija = new sõnaValija(); // sõnaValija klass, et valida suvaline sõna
+        String[] sõnaJaTähendus = sõnaValija.valiSõna(); // String[] sõnast ja selle tähendusest
+        String otsitavSõna = sõnaJaTähendus[0];
+        String otsitavaSõnaTähendus = sõnaJaTähendus[1];
+        int mituTähteSõnas = otsitavSõna.length();
+
+        // TEATAB MITU TÄHTE ARVATAVAS SÕNAS ON
+        System.out.println("Sõnas on " + mituTähteSõnas + " tähte.");
 
         // SCANNERI KLASS
         Scanner sk = new Scanner(System.in);
 
+
         // ESIMENE PAKKUMINE LOOPIST VÄLJAS
         System.out.print("Sinu " + guessCounter + ". pakkumine: ");
-        String arvajaSõna = sk.nextLine();
+        String arvajaSõna = sk.nextLine().toLowerCase();
         guessCounter ++;
 
+
         // LUUAKSE KLASS
-        ÕigedTähed misOnÕigedTähed = new ÕigedTähed(arvajaSõna, sõna);
+        ÕigedTähed misOnÕigedTähed = new ÕigedTähed(arvajaSõna, otsitavSõna);
 
         // LOON KLASSI ABIL DEFAULT VASTUSEMASSIIVI
-        for (int i = 0; i < sõna.length(); i++) {
+        for (int i = 0; i < otsitavSõna.length(); i++) {
             misOnÕigedTähed.lisaTähedMassiivi('_', i);
         }
 
-        if (arvajaSõna.equals(sõna)) { // ERIJUHT KUI KASUTAJA ARVAB ESIMESE KORRAGA SÕNA ÄRA
+        if (arvajaSõna.equals(otsitavSõna)) { // ERIJUHT KUI KASUTAJA ARVAB ESIMESE KORRAGA SÕNA ÄRA
             System.out.println("Arvasid sõna ühe korraga ära! Guesswork or genius?!");
+            System.out.println("Sõna tähendab " + "'" + otsitavaSõnaTähendus + "'");
         } else {
 
             // NÄITAB __A__M__ KUJUL SÕNA, KUS _ ON ARVAMATA TÄHT
@@ -43,11 +49,12 @@ public class Wordle {
             // SENI KUNI KASUTAJA POLE SÕNA ÄRA ARVANUD
             while (!äraArvatud) {
                 System.out.print("Sinu " + guessCounter + ". pakkumine: ");
-                arvajaSõna = sk.nextLine();
+                arvajaSõna = sk.nextLine().toLowerCase();
 
-                if (arvajaSõna.equals(sõna)) { // KASUTAJA ARVAB SÕNA ÄRA
-                    System.out.println("Arvasid sõna " + "'" + sõna + "'" + " ära " + guessCounter + ". korraga. Sõna " + sõna + " tähendus on: " + tähendus);
+                if (arvajaSõna.equals(otsitavSõna)) { // KASUTAJA ARVAB SÕNA ÄRA
+                    System.out.println("Arvasid sõna " + "'" + otsitavSõna + "'" + " ära " + guessCounter + ". korraga.");
                     äraArvatud = true;
+                    System.out.println("Sõna tähendab " + "'" + otsitavaSõnaTähendus + "'");
                 } else {
                     misOnÕigedTähed.setPakutudSõna(arvajaSõna); // UUENDAB SINU GUESSI
                     misOnÕigedTähed.lisaTähed();
